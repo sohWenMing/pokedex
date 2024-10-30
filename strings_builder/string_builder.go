cec81a56a5abaedd42d32c0118fa1dacfcb011d6
nindgabeet <wenming.soh@gmail.com>
2024-10-30 22:01:12 +0800

    got to happy flow test for GeneratePaddedString

--- strings_builder/string_builder.go
+++ strings_builder/string_builder.go
@@ -1,18 +1,26 @@
 package stringBuilder
 
-import "unicode/utf8"
+import (
+	"fmt"
+	"io"
+	"strings"
+	"unicode/utf8"
+)
 
-func GetPadLength(inputString string, desiredLength int) (numToPad int) {
+func GetPadLength(inputString string, desiredLength int) (numToPad int, err error) {
 	numChars := utf8.RuneCountInString(inputString)
 	if desiredLength-numChars < 0 {
-		return 0
+		return 0, fmt.Errorf("desiredLength cannot be less than length of input string")
 	}
-	return desiredLength - numChars
+	return desiredLength - numChars, nil
 }
 
-func GetLeftRightPadLengths(numToPad int) (leftPad, rightPad int) {
-	if numToPad <= 0 {
-		return
+func GetLeftRightPadLengths(numToPad int) (leftPad, rightPad int, err error) {
+	if numToPad < 0 {
+		return 0, 0, fmt.Errorf("numToPad cannot be negative")
+	}
+	if numToPad == 0 {
+		return 0, 0, nil
 	}
 	switch numToPad%2 == 0 {
 	case true:
@@ -22,5 +30,51 @@ func GetLeftRightPadLengths(numToPad int) (leftPad, rightPad int) {
 		leftPad = numToPad / 2
 		rightPad = (numToPad / 2) + 1
 	}
-	return leftPad, rightPad
+	return leftPad, rightPad, nil
+}
+
+/*
+	if i know the left pad, and the right pad
+	determine the character at the edges
+	determine the number of spaces
+*/
+
+func getNumCharsToPad(padLength, numSpaces int) (numCharsToPad int, err error) {
+	if numSpaces > padLength {
+		return 0, fmt.Errorf("numSpaces cannot be greater than padLength")
+	}
+	return padLength - numSpaces, nil
+
+}
+
+func GeneratePaddedString(inputString string, char rune, numSpaces, desiredLength int) (output string, err error) {
+	totalPadLength, err := GetPadLength(inputString, desiredLength)
+	if err != nil {
+		return "", err
+	}
+	leftPadLength, rightPadlength, err := GetLeftRightPadLengths(totalPadLength)
+	if err != nil {
+		return "", err
+	}
+	leftNumChars, err := getNumCharsToPad(leftPadLength, numSpaces)
+	if err != nil {
+		return "", err
+	}
+	rightNumChars, err := getNumCharsToPad(rightPadlength, numSpaces)
+	if err != nil {
+		return "", err
+	}
+
+	leftPadString := strings.Repeat(string(char), leftNumChars) + strings.Repeat(" ", numSpaces)
+	rightPadString := strings.Repeat(" ", numSpaces) + strings.Repeat(string(char), rightNumChars)
+	return leftPadString + inputString + rightPadString, nil
+
+}
+
+func PrintString(w io.Writer, input string) (err error) {
+	_, printErr := fmt.Fprint(w, input)
+	if printErr != nil {
+		return printErr
+	}
+	return nil
 }
--- strings_builder/string_builder_test.go
+++ strings_builder/string_builder_test.go
@@ -1,6 +1,9 @@
 package stringBuilder
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 type GetPadLengthTest struct {
 	name          string
@@ -16,6 +19,23 @@ type GetLeftRightPadLengthsTest struct {
 	rightPad int
 }
 
+type GetNumCharsToPadTest struct {
+	name      string
+	padLength int
+	numSpaces int
+	expected  int
+}
+
+type GeneratePaddedStringTest struct {
+	name          string
+	inputString   string
+	expected      string
+	char          rune
+	numSpaces     int
+	desiredLength int
+}
+
+// ##### TEST FUNCTIONS ##### //
 func TestGetPadLength(t *testing.T) {
 	tests := []GetPadLengthTest{
 		{
@@ -42,24 +62,31 @@ func TestGetPadLength(t *testing.T) {
 			desiredLength: 3,
 			expected:      2,
 		},
-		{
-			name:          "test negative",
-			inputString:   "👀",
-			desiredLength: 0,
-			expected:      0,
-		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got := GetPadLength(test.inputString, test.desiredLength)
+			got, err := GetPadLength(test.inputString, test.desiredLength)
+			assertNoError(err, t)
 			expected := test.expected
 			assertValues(got, expected, t)
 		})
 	}
+
+	negativeTest := GetPadLengthTest{
+		name:          "test negative",
+		inputString:   "👀",
+		desiredLength: 0,
+		expected:      0,
+	}
+	t.Run(negativeTest.name, func(t *testing.T) {
+		_, err := GetPadLength(negativeTest.inputString, negativeTest.desiredLength)
+		assertError(err, t)
+	})
+
 }
 
 func TestGetLeftRightPadLengths(t *testing.T) {
-	tests := []GetLeftRightPadLengthsTest{
+	positiveTests := []GetLeftRightPadLengthsTest{
 		{
 			name:     "testing basic even",
 			numToPad: 4,
@@ -78,19 +105,92 @@ func TestGetLeftRightPadLengths(t *testing.T) {
 			leftPad:  0,
 			rightPad: 0,
 		},
+	}
+	for _, test := range positiveTests {
+		t.Run(test.name, func(t *testing.T) {
+			gotLeft, gotRight, err := GetLeftRightPadLengths(test.numToPad)
+			assertNoError(err, t)
+			assertLeftRightPads(gotLeft, gotRight, test.leftPad, test.rightPad, t)
+		})
+	}
+
+	negativeTest := GetLeftRightPadLengthsTest{
+		name:     "testing negative",
+		numToPad: -1000,
+		leftPad:  0,
+		rightPad: 0,
+	}
+
+	t.Run(negativeTest.name, func(t *testing.T) {
+		_, _, err := GetLeftRightPadLengths(negativeTest.numToPad)
+		assertError(err, t)
+	})
+
+}
+
+func TestGeneratePaddedString(t *testing.T) {
+	positiveTests := []GeneratePaddedStringTest{
 		{
-			name:     "testing negative",
-			numToPad: -1000,
-			leftPad:  0,
-			rightPad: 0,
+			name:          "basic generate padded string test",
+			inputString:   "test",
+			expected:      "#  test  #",
+			char:          '#',
+			numSpaces:     2,
+			desiredLength: 10,
 		},
 	}
-	for _, test := range tests {
+	for _, test := range positiveTests {
 		t.Run(test.name, func(t *testing.T) {
-			gotLeft, gotRight := GetLeftRightPadLengths(test.numToPad)
-			assertLeftRightPads(gotLeft, gotRight, test.leftPad, test.rightPad, t)
-
+			got, err := GeneratePaddedString(test.inputString,
+				test.char, test.numSpaces, test.desiredLength)
+			assertStrings(got, test.expected, t)
+			assertNoError(err, t)
 		})
+	}
+}
+
+func TestPrintString(t *testing.T) {
+	buf := &bytes.Buffer{}
+	input := "this is a test string"
+	err := PrintString(buf, input)
+	assertNoError(err, t)
+	got := buf.String()
+	expected := input
+	assertStrings(got, expected, t)
+}
+
+func TestGetNumCharsToPad(t *testing.T) {
+	positiveTest := GetNumCharsToPadTest{
+		name:      "postive getNumCharsToPad test",
+		padLength: 5,
+		numSpaces: 4,
+		expected:  1,
+	}
+	t.Run(positiveTest.name, func(t *testing.T) {
+		got, err := getNumCharsToPad(positiveTest.padLength, positiveTest.numSpaces)
+		assertNoError(err, t)
+		assertValues(got, positiveTest.expected, t)
+	})
+	negativeTest := GetNumCharsToPadTest{
+		name:      "negative getNumCharsToPad test",
+		padLength: 5,
+		numSpaces: 6,
+		expected:  0,
+	}
+	t.Run(negativeTest.name, func(t *testing.T) {
+		_, err := getNumCharsToPad(negativeTest.padLength, negativeTest.numSpaces)
+		assertError(err, t)
+	})
+}
+
+// ##### END TEST FUNCTIONS ##### //
+
+// ##### ASSERTIONS #####
+
+func assertStrings(got, expected string, t testing.TB) {
+	t.Helper()
+	if got != expected {
+		t.Errorf("\ngot: %s\nwant: %s", got, expected)
 	}
 }
 
@@ -106,3 +206,17 @@ func assertLeftRightPads(gotLeft, gotRight, wantLeft, wantRight int, t testing.T
 			gotLeft, gotRight, wantLeft, wantRight)
 	}
 }
+
+func assertNoError(err error, t testing.TB) {
+	if err != nil {
+		t.Errorf("Unexpected error, %s", err.Error())
+	}
+}
+
+func assertError(err error, t testing.TB) {
+	if err == nil {
+		t.Errorf("expected error, didn't get one")
+	}
+}
+
+// ##### END ASSERTIONS #####

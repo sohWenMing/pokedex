bda92c3cef9700c5d7982a88403b60cd70fa654a
nindgabeet <wenming.soh@gmail.com>
2025-06-01 08:55:42 +0800

    cleaned up parse command, finished explore function with caching

--- command_callbacks/command_callbacks.go
+++ command_callbacks/command_callbacks.go
@@ -2,6 +2,7 @@ package commandcallbacks
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"time"
@@ -16,7 +17,7 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config.Config) error
+	callback    func(c *config.Config, args []string) error
 }
 
 var exitCliCommand = cliCommand{
@@ -40,30 +41,32 @@ var mapbCliCommand = cliCommand{
 	"gets the previous 20 location areas for the Pokemon!",
 	mapbCallBackfunc,
 }
+var exploreCliCommand = cliCommand{
+	"explore",
+	"gets list of all the pokemon that are within the are being explored",
+	exploreCallbackfunc,
+}
 
 var callBackMap map[string]cliCommand = map[string]cliCommand{
-	"exit": exitCliCommand,
-	"help": helpCliCommand,
-	"map":  mapCliCommand,
-	"mapb": mapbCliCommand,
+	"exit":    exitCliCommand,
+	"help":    helpCliCommand,
+	"map":     mapCliCommand,
+	"mapb":    mapbCliCommand,
+	"explore": exploreCliCommand,
 }
 
 func ParseAndExecuteCommand(input string, config *config.Config) error {
-	commandStruct, _ := ParseCommand(input)
+	commandStruct, args := ParseCommand(input)
 	switch commandStruct.name {
 	case "exit":
-		commandStruct.callback(config)
+		commandStruct.callback(config, args)
 		return nil
-	case "map":
-		err := commandStruct.callback(config)
+	default:
+		err := commandStruct.callback(config, args)
 		if err != nil {
 			fmt.Println("A problem occured when trying to get the information. Please try again")
 			fmt.Println(err)
 		}
-		return nil
-
-	default:
-		commandStruct.callback(config)
 		return nil
 	}
 }
@@ -81,13 +84,13 @@ func ParseCommand(input string) (commandStruct cliCommand, args []string) {
 	return callBack, cleanedInput[1:]
 }
 
-func exitCallBackfunc(*config.Config) error {
+func exitCallBackfunc(c *config.Config, args []string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	time.Sleep(1 * time.Second)
 	os.Exit(0)
 	return nil
 }
-func helpCallBackfunc(*config.Config) error {
+func helpCallBackfunc(c *config.Config, args []string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println("")
@@ -95,7 +98,7 @@ func helpCallBackfunc(*config.Config) error {
 	fmt.Println("exit: Exit the Pokedex")
 	return nil
 }
-func mapCallBackfunc(c *config.Config) error {
+func mapCallBackfunc(c *config.Config, args []string) error {
 	c.IncOffset()
 	err := getLocationsAreas(c)
 	if err != nil {
@@ -104,7 +107,7 @@ func mapCallBackfunc(c *config.Config) error {
 	return nil
 }
 
-func mapbCallBackfunc(c *config.Config) error {
+func mapbCallBackfunc(c *config.Config, args []string) error {
 	c.DecOffSet()
 	if c.GetOffSet() < 0 {
 		c.ResetOffSet()
@@ -115,6 +118,34 @@ func mapbCallBackfunc(c *config.Config) error {
 		return err
 	}
 	return nil
+}
+func exploreCallbackfunc(c *config.Config, args []string) error {
+	if len(args) != 1 {
+		return errors.New("number of argumens passed into explore must be only 1")
+	}
+	urlKey := mapExploreUrl(args[0])
+	err := getExploreResult(c, urlKey)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+func getExploreResult(c *config.Config, urlKey string) error {
+	isFound, cacheEntry := getFromCache(c.GetCache(), urlKey)
+	if isFound {
+		utils.WriteLine(c.Writer, "Retrieving information from cache")
+		pokemon := cacheEntry.WriteBufToStrings()
+		writeLocsFromCachedLocations(c, pokemon)
+		return nil
+
+	} else {
+		err := callExplore(c, urlKey)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
 }
 
 func getLocationsAreas(c *config.Config) error {
@@ -159,11 +190,43 @@ func callLocAreas(c *config.Config, url string) error {
 	}
 	return nil
 }
+func callExplore(c *config.Config, url string) error {
+
+	res, err := c.GetClient().Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	var locResult structdefinitions.LocationResult
+	decoder := json.NewDecoder(res.Body)
+	jsonErr := decoder.Decode(&locResult)
+	if jsonErr != nil {
+		return jsonErr
+	}
+	writePokemonFromExploreResults(c, locResult)
+	err = cacheExploreResults(c, url, locResult)
+	if err != nil {
+		deleteErrorWriteFromCache(c, url)
+	}
+	return nil
+}
 
 func cacheLocationAreaResults(c *config.Config, url string, locaAreaResult structdefinitions.LocationAreaResult) error {
 	names := make([]string, 0, len(locaAreaResult.Results))
 	for _, result := range locaAreaResult.Results {
 		name := result.Name
+		names = append(names, name)
+	}
+	err := c.GetCache().WriteToCache(url, names)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func cacheExploreResults(c *config.Config, url string, locaAreaResult structdefinitions.LocationResult) error {
+	names := make([]string, 0, len(locaAreaResult.PokemonEncounters))
+	for _, result := range locaAreaResult.PokemonEncounters {
+		name := result.Pokemon.Name
 		names = append(names, name)
 	}
 	err := c.GetCache().WriteToCache(url, names)
@@ -179,6 +242,19 @@ func deleteErrorWriteFromCache(c *config.Config, key string) {
 		return
 	}
 	delete(c.GetCache().AccessCacheMap(), key)
+}
+func writePokemonFromExploreResults(c *config.Config, locationResult structdefinitions.LocationResult) {
+	header := "##### Pokemon Found Start #####"
+	footer := "##### Pokemon Found End   #####"
+	utils.WriteLine(c.Writer, "")
+	utils.WriteLine(c.Writer, header)
+	utils.WriteLine(c.Writer, "")
+	for _, result := range locationResult.PokemonEncounters {
+		utils.WriteLine(c.Writer, result.Pokemon.Name)
+	}
+	utils.WriteLine(c.Writer, "")
+	utils.WriteLine(c.Writer, footer)
+	utils.WriteLine(c.Writer, "")
 }
 
 func writeLocsFromLocationAreaResult(c *config.Config, locationAreaResult structdefinitions.LocationAreaResult) {
@@ -207,6 +283,12 @@ func writeLocsFromCachedLocations(c *config.Config, locations []string) {
 	utils.WriteLine(c.Writer, "")
 	utils.WriteLine(c.Writer, footer)
 	utils.WriteLine(c.Writer, "")
+
+}
+
+func mapExploreUrl(param string) string {
+	requesturl := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/%s", param)
+	return requesturl
 
 }
 
--- integration_testing/integration_test.go
+++ integration_testing/integration_test.go
@@ -1,7 +1,10 @@
 package integrationtesting
 
 import (
+	"bytes"
+	"fmt"
 	"io"
+	"strings"
 	"testing"
 	"time"
 
@@ -28,5 +31,62 @@ func TestResetOffset(t *testing.T) {
 	want := 0
 	if got != want {
 		t.Errorf("\ngot: %d\nwant: %d", got, want)
+	}
+}
+func TestExplore(t *testing.T) {
+	areas := []string{
+		// "canalave-city-area",
+		// "eterna-city-area",
+		// "pastoria-city-area",
+		// "sunyshore-city-area",
+		// "sinnoh-pokemon-league-area",
+		// "oreburgh-mine-1f",
+		// "oreburgh-mine-b1f",
+		// "valley-windworks-area",
+		// "eterna-forest-area",
+		// "fuego-ironworks-area",
+		// "mt-coronet-1f-route-207",
+		// "mt-coronet-2f",
+		// "mt-coronet-3f",
+		// "mt-coronet-exterior-snowfall",
+		// "mt-coronet-exterior-blizzard",
+		// "mt-coronet-4f",
+		// "mt-coronet-4f-small-room",
+		// "mt-coronet-5f",
+		// "mt-coronet-6f",
+		"mt-coronet-1f-from-exterior",
+	}
+	buf := &bytes.Buffer{}
+	expectedPokemon := []string{
+		"clefairy",
+		"golbat",
+		"machoke",
+		"graveler",
+		"nosepass",
+		"medicham",
+		"lunatone",
+		"solrock",
+		"chingling",
+		"bronzong",
+	}
+
+	config, err := config.InitConfig(buf, 1*time.Second, 1*time.Second)
+	if err != nil {
+		t.Errorf("\ndidn't expect error, got %v", err)
+	}
+	config.SetClient(httputils.InitClient())
+
+	for _, area := range areas {
+		command := fmt.Sprintf("explore %s", area)
+
+		err = commandcallbacks.ParseAndExecuteCommand(command, &config)
+		if err != nil {
+			t.Errorf("\ndidn't expect error, got %v", err)
+		}
+	}
+	for _, pokemon := range expectedPokemon {
+		if !strings.Contains(buf.String(), pokemon) {
+			t.Errorf("\n%s was expected but not found", pokemon)
+		}
 	}
 }
